17ea81db8859e9c10231774dd6fd79577c8446c1
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-09-26 16:49:49 -0700

    UpdatePrayerCount Lambda Function
    Updated lambda function to fix the errors I was getting back from the
    API regarding incorrect schema. It turns out you need to send the
    primary and sort key when updating a DynamoDB with a sort key. I also
    created a new layer in Lambda for the imports. This isn't part of the
    project source code.

--- aws/lambda/update-prayer-count/index.js
+++ aws/lambda/update-prayer-count/index.js
@@ -1,22 +1,31 @@
-import { UpdateItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 
 const dynamodb = new DynamoDBClient();
+const dynamodbClient = DynamoDBDocumentClient.from(dynamodb);
 const tableName = 'ArdentPrayers_Dev';
 
 export const handler = async (event) => {
-	const command = new UpdateItemCommand({
+	console.log(`Payload: \n ID - ${event.id}, \n Date - ${event.date} \n Count - ${event.count}`);
+
+	/**
+	 * When you have a DynamoDB database that is created with a sort key you have to include it
+	 * in the `Key` object otherwise you'll keep getting the dreaded "key doesn't match your schema" error.
+	 */
+	const command = new UpdateCommand({
 		TableName: tableName,
 		Key: {
-			PrayerId: { S: event.id }
+			PrayerId: event.id,
+			CreatedDate: event.date
 		},
-		UpdateExpression: 'set Count = :newCount',
+		UpdateExpression: 'set PrayerCount = :newCount',
 		ExpressionAttributeValues: {
-			':newCount': { NUM: event.count }
+			':newCount': event.count
 		},
 		ReturnValues: 'ALL_NEW'
 	});
 
-	const response = await dynamodb.send(command);
+	const response = await dynamodbClient.send(command);
 	console.log(response);
 	return response;
 };
