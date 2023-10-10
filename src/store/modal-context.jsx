1bfc2c56474932c6e658b4f1d2b92611b01a03dd
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-10-10 11:25:53 -0700

    Modal Fade-In
    Updated modal styles to fade in the backdrop. I didn't add a style to
    fade out as its pointless because I'm removing the `position: fixed`.
    This was by design and I left a todo to address and expand the modal
    context.

--- src/store/modal-context.jsx
+++ src/store/modal-context.jsx
@@ -1,3 +1,9 @@
+/**
+ * The modal-context is crude in that it just simply toggles a boolean to show or display. A future iteration
+ * should allow an in between stage when it closes.
+ * @todo Extend modal to fade in/out and removing itself as to not affect other dom elements
+ */
+
 import React, { useState } from 'react';
 
 const ModalContext = React.createContext({
