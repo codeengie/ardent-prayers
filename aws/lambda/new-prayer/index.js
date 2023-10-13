808f63e1b2cef01b11656d9e059fa12fa5f810ed
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-10-13 15:06:09 -0700

    Recaptcha Response Token Verification
    Updated the `new-prayer` lambda function to first verify the user's
    recaptcha token before inserting the form data into the database.

--- aws/lambda/new-prayer/index.js
+++ aws/lambda/new-prayer/index.js
@@ -33,22 +33,33 @@ export const handler = async (event) => {
 	let statusCode = '200';
 
 	try {
-		/**
-		 * Inserting a new item with JS v3 API does not return an object with the data you inserted or the typical
-		 * object you would get when using fetch(). Instead, it returns an object with a `$metadata` object that
-		 * contains these properties:
-		 * 1. httpStatuscode
-		 * 2. requestId
-		 * 3. attempts
-		 * 4. totalRetryDelay
-		 */
-		const response = await dynamodbClient.send(command);
+		// Before we can insert the payload into the database let's verify the user's recaptcha token
+		const recaptcha = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${event.formToken}`);
+		const recaptchaData = await recaptcha.json();
 
-		if (response.$metadata.httpStatusCode !== 200) {
-			new Error(`Prayer request could not be added: ${response.status}`);
+		// Check the response status, if the user is human proceed to sending the payload to the database
+		if (recaptchaData.success) {
+			console.log('Recaptcha, human verified');
+			/**
+			 * Inserting a new item with JS v3 API does not return an object with the data you inserted or the typical
+			 * object you would get when using fetch(). Instead, it returns an object with a `$metadata` object that
+			 * contains these properties:
+			 * 1. httpStatuscode
+			 * 2. requestId
+			 * 3. attempts
+			 * 4. totalRetryDelay
+			 */
+			const response = await dynamodbClient.send(command);
+
+			if (response.$metadata.httpStatusCode !== 200) {
+				new Error(`Prayer request could not be added: ${response.status}`);
+			}
+
+			body = dbData;
+		} else {
+			console.log('Bot detected!');
+			new Error('Error verifying reCAPTCHA');
 		}
-
-		body = dbData;
 
 	} catch (error) {
 		statusCode = '400';
