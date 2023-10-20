732dfc46e4e75059e35bbb8ccd718c0c17378950
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-10-19 19:38:33 -0700

    Hero Component Image Refactor
    Refactored the hero component to display the background image inline
    rather than using css to display it. I did this to increase the
    pagespeed score. The assumption here is I was getting penalized for the
    delay of the image displaying because the css needs to load first.
    
    Coding this was a bit trickier because I used Vite's `import.meta.glob`
    to import all the hero images versus importing individually. In a small
    project like this it probably would be ok but at scale it would be an
    elegant solution.
    
    I'm going to revise this component again to make it reusable.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,43 @@
 import './Hero.scss';
 
+/**
+ * Import all hero images
+ * @link https://vitejs.dev/guide/features.html#glob-import
+ */
+const images = import.meta.glob('../../assets/images/hero-*w.webp');
+const breakpoints = [];
+
+// Iterate over the keys of the image object
+for (const path in images) {
+	images[path]().then(image => {
+		let imgData = {};
+		const matched = image.default.match(/\d+/); // `match()` returns an array
+
+		imgData.width = matched[0];
+		imgData.path = image.default;
+
+		breakpoints.push(imgData);
+	});
+}
+
 const Hero = () => {
+	const imageSizes = breakpoints
+		.map(breakpoint => `(max-width: ${breakpoint.width}px) ${breakpoint.width}w`).toString();
+	const imageSources = breakpoints
+		.map(breakpoint => `${breakpoint.path} ${breakpoint.width}w`).toString();
+	const fallback = breakpoints.find(element => element.width === '1200');
+
 	return (
 		<div className="hero">
+			<img
+				alt="A man praying with his head down, hands closed together with fingers crossed."
+				className="hero__bg"
+				height="200"
+				sizes={imageSizes}
+				srcSet={imageSources}
+				src={fallback.path}
+				width="286"
+			/>
 			<blockquote className="hero__verse">
 				<p className="hero__text">Don&rsquo;t worry about anything; instead, pray about everything.</p>
 				<cite className="hero__book">Philippians 4:6 NLT</cite>
